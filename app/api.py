81b79a8510f9b7f2892787adc447910fdb33427c
Sanchit Balchandani <sanchit.balchandani@britishgas.co.uk>
2023-03-30 03:48:34 +0530

    chore(ci): fix cirlce ci for tests v2

--- app/api.py
+++ app/api.py
@@ -1,14 +1,17 @@
 from flask import jsonify
 from app import app
 
+
 @app.route("/")
 def index():
     return "Hello, World!"
+
 
 @app.route("/api")
 def api():
     data = {"name": "John Doe", "age": 30, "city": "New York"}
     return jsonify(data)
+
 
 if __name__ == "__main__":
     app.run()
