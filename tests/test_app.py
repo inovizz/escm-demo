e8591139f5d7f2d4a0c0fd51eadc0dff738c8050
Sanchit Balchandani <sanchit.balchandani@britishgas.co.uk>
2023-03-30 03:17:05 +0530

    chore(misc): fix black formatting

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,20 +2,23 @@ import os
 import pytest
 from app import app
 
+
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
 
+
 def test_index(client):
-    response = client.get('/')
-    assert b'Hello, World!' in response.data
+    response = client.get("/")
+    assert b"Hello, World!" in response.data
+
 
 def test_api(client):
-    response = client.get('/api')
+    response = client.get("/api")
     assert response.status_code == 200
     assert response.is_json
     data = response.get_json()
-    assert 'name' in data
-    assert 'age' in data
-    assert 'city' in data
+    assert "name" in data
+    assert "age" in data
+    assert "city" in data
